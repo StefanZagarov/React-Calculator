fcd1f0bef1db87aa67e50f89c031d8c2d120e042
StefanZagarov <stefanzagarov@gmail.com>
2025-05-22 14:42:17 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="container">
+      <div className="calculator">
+        <button className='btn'>1</button>
+        <button className='btn'>1</button>
+        <button className='btn'>1</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
