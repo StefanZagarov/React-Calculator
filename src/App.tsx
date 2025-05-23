8699eaa45708cb5d9691a6b51276d338ea6210f1
StefanZagarov <stefanzagarov@gmail.com>
2025-05-23 18:38:23 +0300

    Button text scales with size

--- src/App.tsx
+++ src/App.tsx
@@ -45,32 +45,32 @@ function App() {
             <Button value='7' onClick={handleClick} />
             <Button value='8' onClick={handleClick} />
             <Button value='9' onClick={handleClick} />
-            <Button variant="function" value='/' onClick={handleClick} />
-            <Button variant="function" value='*' onClick={handleClick} />
+            <Button variant="function" value='C' onClick={handleClick} />
+            <Button variant="function" value='%' onClick={handleClick} />
           </div>
 
           <div className="row">
             <Button value='4' onClick={handleClick} />
             <Button value='5' onClick={handleClick} />
             <Button value='6' onClick={handleClick} />
-            <Button variant="function" value='(' onClick={handleClick} />
-            <Button variant="function" value='+' onClick={handleClick} />
+            <Button variant="function" value='*' onClick={handleClick} />
+            <Button variant="function" value='/' onClick={handleClick} />
           </div>
 
           <div className="row">
             <Button value='1' onClick={handleClick} />
             <Button value='2' onClick={handleClick} />
             <Button value='3' onClick={handleClick} />
-            <Button variant="function" value='C' onClick={handleClick} />
-            <Button variant="function" value='%' onClick={handleClick} />
+            <Button variant="function" value='+' onClick={handleClick} />
+            <Button variant="function" value='-' onClick={handleClick} />
           </div>
 
           <div className="row">
             <Button value='.' onClick={handleClick} />
             <Button value='0' onClick={handleClick} />
             <Button variant="function" value='+/-' onClick={handleClick} />
+            <Button variant="function" value='(' onClick={handleClick} />
             <Button variant="function" value=')' onClick={handleClick} />
-            <Button variant="function" value='-' onClick={handleClick} />
           </div>
         </div>
       </div>
