60116df0a3f823f009cd6391e842477d32614a9d
StefanZagarov <stefanzagarov@gmail.com>
2025-05-25 20:21:46 +0300

    Improved caret logic; Added operator inputs

--- src/App.tsx
+++ src/App.tsx
@@ -18,43 +18,83 @@ const LEFT_BRACKET = `(`;
 const RIGHT_BRACKET = `)`;
 const MOVE_CARET_RIGHTMOST = `>>`;
 
+const operators = [PLUS, MINUS, TIMES, DIVIDE, PERCENT];
+
 function App() {
-  const [input, setInput] = useState<string>(`0`);
+  const [inputScreen, setInputScreen] = useState<string>(`0`);
   const [result, setResult] = useState<string>(`0`);
   // App now owns the desired caret position state
   const [caretPosition, setCaretPosition] = useState<number>(1);
 
+  function setNewCaretPosition(newCaretPosition: number) {
+    setCaretPosition(newCaretPosition);
+  }
+
   // Callback for CalcScreen to inform App of user-driven changes
-  function handleUserInputChangeInScreen(newInputValue: string, newCaretPos: number) {
-    setInput(newInputValue);
-    setCaretPosition(newCaretPos);
+  function handleKeyboardInput(value: string) {
+    handleNewInput(value);
   };
 
-  function handleClick(value: string) {
-    const currentCaretPosition = caretPosition;
-    let newInput = input;
+  function resolveNewInputResult(value: string, currentCaretPosition: number) {
+    const partBeforeCaret = inputScreen.slice(0, currentCaretPosition);
+    const partAfterCaret = inputScreen.slice(currentCaretPosition);
+    const newInputValue = partBeforeCaret + value + partAfterCaret;
 
-    switch (value) {
+    setInputScreen(newInputValue);
+    setCaretPosition(currentCaretPosition + 1);
+  }
+
+  function isLeftOfCaretEmpty(): boolean {
+    return inputScreen[caretPosition - 1] === undefined;
+  }
+
+  function resolveOperatorResult(inputValue: string, caretPosition: number) {
+    if (operators.includes(inputScreen[caretPosition - 1])) {
+      const partBeforeCaret = inputScreen.slice(0, caretPosition - 1);
+      const partAfterCaret = inputScreen.slice(caretPosition);
+      const newInputValue = partBeforeCaret + inputValue + partAfterCaret;
+
+      setInputScreen(newInputValue);
+      // Keep caret position, workaround for a weird behavior causing it to go rightmost:
+      // Force change
+      setCaretPosition(caretPosition - 1);
+      // Then restore
+      requestAnimationFrame(() => {
+        setCaretPosition(caretPosition);
+      });
+    }
+    else {
+      resolveNewInputResult(inputValue, caretPosition);
+    };
+  }
+
+  // TODO: Make result screen to automatically calculate input field's data, if an operator is present
+
+  function handleNewInput(inputValue: string) {
+    const currentCaretPosition = caretPosition;
+    let newInput = inputScreen;
+
+    switch (inputValue) {
       case C:
-        setInput(`0`);
+        setInputScreen(`0`);
         setResult(`0`);
         return;
 
       case BACKSPACE:
         if (currentCaretPosition === 0) return;
-        newInput = input.slice(0, currentCaretPosition - 1) + input.slice(currentCaretPosition);
+        newInput = inputScreen.slice(0, currentCaretPosition - 1) + inputScreen.slice(currentCaretPosition);
 
         if (newInput === "") {
-          setInput('0');
+          setInputScreen('0');
         }
         else {
-          setInput(newInput);
+          setInputScreen(newInput);
           setCaretPosition(Math.max(0, currentCaretPosition - 1));
         }
         return;
 
       case MOVE_CARET_RIGHTMOST:
-        setCaretPosition(input.length);
+        setCaretPosition(inputScreen.length);
         return;
 
       case EQUAL:
@@ -62,54 +102,61 @@ function App() {
         return;
 
       case PERCENT:
-        console.log("To be implemented");
+        if (isLeftOfCaretEmpty()) return;
+        resolveOperatorResult(inputValue, currentCaretPosition);
         return;
 
       case NEGATE:
+        if (isLeftOfCaretEmpty()) return;
+
         console.log("To be implemented");
         return;
 
       case LEFT_BRACKET:
+        if (isLeftOfCaretEmpty()) return;
+        resolveNewInputResult(inputValue, currentCaretPosition);
         console.log("To be implemented");
         return;
 
       case RIGHT_BRACKET:
+        if (isLeftOfCaretEmpty()) return;
+        resolveNewInputResult(inputValue, currentCaretPosition);
         console.log("To be implemented");
         return;
 
       case PLUS:
+        if (isLeftOfCaretEmpty()) return;
+        resolveOperatorResult(inputValue, currentCaretPosition);
+
         console.log("To be implemented");
         return;
 
       case MINUS:
+        if (isLeftOfCaretEmpty()) return;
+        resolveOperatorResult(inputValue, currentCaretPosition);
         console.log("To be implemented");
         return;
 
       case TIMES:
+        if (isLeftOfCaretEmpty()) return;
+        resolveOperatorResult(inputValue, currentCaretPosition);
         console.log("To be implemented");
         return;
 
       case DIVIDE:
+        if (isLeftOfCaretEmpty()) return;
+        resolveOperatorResult(inputValue, currentCaretPosition);
         console.log("To be implemented");
         return;
-
-      default:
-        console.log("Invalid input");
-        break;
     }
 
-    if (input === `0` && value !== `0`) {
-      setInput(value);
+    if (inputScreen === `0` && caretPosition === 1 && inputValue !== "0") {
+      setInputScreen(inputValue);
       setCaretPosition(currentCaretPosition + 1);
       return;
     }
 
-    const partBeforeCaret = input.slice(0, currentCaretPosition);
-    const partAfterCaret = input.slice(currentCaretPosition);
-    const newInputValue = partBeforeCaret + value + partAfterCaret;
-
-    setInput(newInputValue);
-    setCaretPosition(currentCaretPosition + 1);
+    resolveNewInputResult(inputValue, currentCaretPosition);
   }
 
   return (
@@ -118,50 +165,51 @@ function App() {
 
         <div className="screen">
           <CalcScreen
-            input={input}
+            inputScreen={inputScreen}
             result={result}
-            cursorPosition={caretPosition}
-            onUserInputChange={handleUserInputChangeInScreen}
+            caretPosition={caretPosition}
+            onKeyboardInput={handleKeyboardInput}
+            onCaretPositionChange={setNewCaretPosition}
           />
         </div>
 
         <div className="buttons">
           <div className="row">
-            <Button variant="function-top" value='<--' onClick={handleClick} />
-            <Button variant="function-top" value='=' onClick={handleClick} />
-            <Button variant="function-top" value='>>' onClick={handleClick} />
+            <Button variant="function-top" value='<--' onClick={handleNewInput} />
+            <Button variant="function-top" value='=' onClick={handleNewInput} />
+            <Button variant="function-top" value='>>' onClick={handleNewInput} />
           </div>
 
           <div className="row">
-            <Button value='7' onClick={handleClick} />
-            <Button value='8' onClick={handleClick} />
-            <Button value='9' onClick={handleClick} />
-            <Button variant="function" value='%' onClick={handleClick} />
-            <Button variant="function" value='C' onClick={handleClick} />
+            <Button value='7' onClick={handleNewInput} />
+            <Button value='8' onClick={handleNewInput} />
+            <Button value='9' onClick={handleNewInput} />
+            <Button variant="function" value='%' onClick={handleNewInput} />
+            <Button variant="function" value='C' onClick={handleNewInput} />
           </div>
 
           <div className="row">
-            <Button value='4' onClick={handleClick} />
-            <Button value='5' onClick={handleClick} />
-            <Button value='6' onClick={handleClick} />
-            <Button variant="function" value='*' onClick={handleClick} />
-            <Button variant="function" value='/' onClick={handleClick} />
+            <Button value='4' onClick={handleNewInput} />
+            <Button value='5' onClick={handleNewInput} />
+            <Button value='6' onClick={handleNewInput} />
+            <Button variant="function" value='*' onClick={handleNewInput} />
+            <Button variant="function" value='/' onClick={handleNewInput} />
           </div>
 
           <div className="row">
-            <Button value='1' onClick={handleClick} />
-            <Button value='2' onClick={handleClick} />
-            <Button value='3' onClick={handleClick} />
-            <Button variant="function" value='+' onClick={handleClick} />
-            <Button variant="function" value='-' onClick={handleClick} />
+            <Button value='1' onClick={handleNewInput} />
+            <Button value='2' onClick={handleNewInput} />
+            <Button value='3' onClick={handleNewInput} />
+            <Button variant="function" value='+' onClick={handleNewInput} />
+            <Button variant="function" value='-' onClick={handleNewInput} />
           </div>
 
           <div className="row">
-            <Button value='.' onClick={handleClick} />
-            <Button value='0' onClick={handleClick} />
-            <Button variant="function" value='+/-' onClick={handleClick} />
-            <Button variant="function" value='(' onClick={handleClick} />
-            <Button variant="function" value=')' onClick={handleClick} />
+            <Button value='.' onClick={handleNewInput} />
+            <Button value='0' onClick={handleNewInput} />
+            <Button variant="function" value='+/-' onClick={handleNewInput} />
+            <Button variant="function" value='(' onClick={handleNewInput} />
+            <Button variant="function" value=')' onClick={handleNewInput} />
           </div>
         </div>
       </div>
